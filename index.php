b516b828e8f92e6cc1ad9255bf3d11bb9d983ad0
sanchezmip <pododolskiy28@gmail.com>
2026-05-04 02:11:59 +0300

    лаба 13

--- index.php
+++ index.php
@@ -1,82 +1,77 @@
 <?php
-echo "<h1>Лабораторная работа 12</h1>";
+echo "<h1>Лабораторная работа 13</h1>";
 
-echo "<h2>Часть 1</h2>";
+class Worker
+{
+    private $name;
+    private $age;
+    private $salary;
 
-// 1
-echo "<h3>1. Открытие несуществующего файла</h3>";
-try {
-    $file = fopen("no_file.txt", "r");
-} catch (Exception $ex) {
-    echo "Ошибка: " . $ex->getMessage() . "<br>";
-}
-
-// 2
-echo "<h3>2. Деление на ноль</h3>";
-try {
-    $a = 10;
-    $b = 0;
-    if ($b == 0) {
-        throw new Exception("Деление на ноль!");
+    public function __construct($name, $age, $salary)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->salary = $salary;
     }
-    $res = $a / $b;
-} catch (Exception $ex) {
-    echo "Ошибка: " . $ex->getMessage() . "<br>";
-    file_put_contents("log.txt", $ex->getMessage());
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function getSalary()
+    {
+        return $this->salary;
+    }
+
+    private function checkAge($age)
+    {
+        return $age >= 18;
+    }
+
+    public function setAge($age)
+    {
+        if ($this->checkAge($age)) {
+            $this->age = $age;
+            echo "Возраст изменен на $age<br>";
+        } else {
+            echo "Вам работать в нашей компании еще рано<br>";
+        }
+    }
 }
 
-// 3
-echo "<h3>3. Несуществующий элемент массива</h3>";
-$countries = ['Spain' => 'Madrid', 'Russia' => 'Moscow'];
-try {
-    echo $countries['Germany'];
-} catch (Exception $ex) {
-    echo "Ошибка: " . $ex->getMessage() . "<br>";
-}
+$worker1 = new Worker("Иван Петров", 25, 50000);
+$worker2 = new Worker("Мария Сидорова", 30, 70000);
 
-echo "<h2>Часть 2</h2>";
+echo "<h2>Информация о работниках</h2>";
+echo "Работник 1: " . $worker1->getName() . ", возраст: " . $worker1->getAge() . ", зарплата: " . $worker1->getSalary() . "<br>";
+echo "Работник 2: " . $worker2->getName() . ", возраст: " . $worker2->getAge() . ", зарплата: " . $worker2->getSalary() . "<br>";
 
-// 1
-echo "1. " . mktime(10, 25, 0, 3, 15, 2025) . "<br>";
+$sumSalary = $worker1->getSalary() + $worker2->getSalary();
+$sumAge = $worker1->getAge() + $worker2->getAge();
+echo "<h2>Суммы</h2>";
+echo "Сумма зарплат: $sumSalary<br>";
+echo "Сумма возрастов: $sumAge<br>";
 
-// 2
-$old = mktime(8, 5, 59, 10, 2, 1990);
-echo "2. " . (time() - $old) . " секунд<br>";
+echo "<h2>Сумма зарплат через getSalary</h2>";
+echo "Сумма зарплат: " . ($worker1->getSalary() + $worker2->getSalary()) . "<br>";
 
-// 3
-echo "3. " . date('Y.m.d H:i:s') . "<br>";
+echo "<h2>Проверка setAge</h2>";
+echo "Текущий возраст Ивана: " . $worker1->getAge() . "<br>";
+$worker1->setAge(20);
+echo "Новый возраст Ивана: " . $worker1->getAge() . "<br>";
+$worker1->setAge(16);
+echo "Возраст после попытки установить 16: " . $worker1->getAge() . "<br>";
 
-// 4
-echo "4. " . date('Y.m.d', mktime(0, 0, 0, 9, 1)) . "<br>";
-
-// 5
-$week = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
-echo "5. " . $week[date('w', mktime(0, 0, 0, 2, 2, 2000))] . "<br>";
-
-// 6
-echo "6. Сегодня: " . $week[date('w')] . "<br>";
-
-// 7
-echo "<h3>7. Сравнение дат</h3>";
-echo '<form method="post">
-    Дата 1: <input type="date" name="d1"><br>
-    Дата 2: <input type="date" name="d2"><br>
-    <input type="submit" name="comp" value="Сравнить">
-</form>';
-if(isset($_POST['comp'])){
-    if($_POST['d1'] > $_POST['d2']) echo "Больше: " . $_POST['d1'];
-    else echo "Больше: " . $_POST['d2'];
-}
-
-// 8
-echo "<br>8. " . date('d-m-Y', strtotime('2025-12-31')) . "<br>";
-
-// 9
-$date = date_create('2000-02-03');
-date_modify($date, '+2 days');
-echo "9. +2 дня: " . date_format($date, 'd.m.Y') . "<br>";
-
-// 10
-$ny = mktime(0,0,0,1,1,date('Y')+1);
-echo "10. До НГ: " . ceil(($ny - time())/86400) . " дней";
+echo "<h2>Проверка возраста через setAge</h2>";
+echo "Устанавливаем возраст 25 для Марии: ";
+$worker2->setAge(25);
+echo "Устанавливаем возраст 15 для Марии: ";
+$worker2->setAge(15);
+echo "Итоговый возраст Марии: " . $worker2->getAge() . "<br>";
 ?>
